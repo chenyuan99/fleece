0a0b0efd6f087e820ea92fa986ad1540bb236ed9
Yuan Chen <cysbc1999@gmail.com>
2024-05-04 23:55:52 -0400

    update code quality tool

--- fleece.py
+++ fleece.py
@@ -13,7 +13,7 @@ from langchain.chains.conversation.prompt import ENTITY_MEMORY_CONVERSATION_TEMP
 from langchain.llms import OpenAI
 
 # Set Streamlit page configuration
-st.set_page_config(page_title='🧠MemoryBot🤖', layout='wide')
+st.set_page_config(page_title="Fleece", layout="wide")
 # Initialize session states
 if "generated" not in st.session_state:
     st.session_state["generated"] = []
@@ -24,6 +24,7 @@ if "input" not in st.session_state:
 if "stored_session" not in st.session_state:
     st.session_state["stored_session"] = []
 
+
 # Define function to get user input
 def get_text():
     """
@@ -32,10 +33,15 @@ def get_text():
     Returns:
         (str): The text entered by the user
     """
-    input_text = st.text_input("You: ", st.session_state["input"], key="input",
-                            placeholder="Your AI assistant here! Ask me anything ...", 
-                            label_visibility='hidden')
+    input_text = st.text_input(
+        "You: ",
+        st.session_state["input"],
+        key="input",
+        placeholder="Your AI assistant here! Ask me anything ...",
+        label_visibility="hidden",
+    )
     return input_text
+
 
 # Define function to start a new chat
 def new_chat():
@@ -43,15 +49,16 @@ def new_chat():
     Clears session state and starts a new chat.
     """
     save = []
-    for i in range(len(st.session_state['generated'])-1, -1, -1):
+    for i in range(len(st.session_state["generated"]) - 1, -1, -1):
         save.append("User:" + st.session_state["past"][i])
-        save.append("Bot:" + st.session_state["generated"][i])        
+        save.append("Bot:" + st.session_state["generated"][i])
     st.session_state["stored_session"].append(save)
     st.session_state["generated"] = []
     st.session_state["past"] = []
     st.session_state["input"] = ""
     st.session_state.entity_memory.entity_store = {}
     st.session_state.entity_memory.buffer.clear()
+
 
 # Set up sidebar with various options
 with st.sidebar.expander("🛠️ ", expanded=False):
@@ -63,12 +70,22 @@ with st.sidebar.expander("🛠️ ", expanded=False):
     if st.checkbox("Preview memory buffer"):
         with st.expander("Bufffer-Store", expanded=False):
             st.session_state.entity_memory.buffer
-    MODEL = st.selectbox(label='Model', options=['gpt-3.5-turbo','text-davinci-003','text-davinci-002','code-davinci-002'])
-    K = st.number_input(' (#)Summary of prompts to consider',min_value=3,max_value=1000)
+    MODEL = st.selectbox(
+        label="Model",
+        options=[
+            "gpt-3.5-turbo",
+            "text-davinci-003",
+            "text-davinci-002",
+            "code-davinci-002",
+        ],
+    )
+    K = st.number_input(
+        " (#)Summary of prompts to consider", min_value=3, max_value=1000
+    )
 
 # Set up the Streamlit app layout
-st.title("🤖 Chat Bot with 🧠")
-st.subheader(" Powered by 🦜 LangChain + OpenAI + Streamlit")
+st.title("Fleece")
+st.subheader(" find the best card for deal saviors")
 
 # Ask the user to enter their OpenAI API key
 API_O = st.sidebar.text_input("API-KEY", type="password")
@@ -76,60 +93,57 @@ API_O = st.sidebar.text_input("API-KEY", type="password")
 # Session state storage would be ideal
 if API_O:
     # Create an OpenAI instance
-    llm = OpenAI(temperature=0,
-                openai_api_key=API_O, 
-                model_name=MODEL, 
-                verbose=False) 
-
+    llm = OpenAI(temperature=0, openai_api_key=API_O, model_name=MODEL, verbose=False)
 
     # Create a ConversationEntityMemory object if not already created
-    if 'entity_memory' not in st.session_state:
-            st.session_state.entity_memory = ConversationEntityMemory(llm=llm, k=K )
-        
-        # Create the ConversationChain object with the specified configuration
+    if "entity_memory" not in st.session_state:
+        st.session_state.entity_memory = ConversationEntityMemory(llm=llm, k=K)
+
+    # Create the ConversationChain object with the specified configuration
     Conversation = ConversationChain(
-            llm=llm, 
-            prompt=ENTITY_MEMORY_CONVERSATION_TEMPLATE,
-            memory=st.session_state.entity_memory
-        )  
+        llm=llm,
+        prompt=ENTITY_MEMORY_CONVERSATION_TEMPLATE,
+        memory=st.session_state.entity_memory,
+    )
 else:
-    st.sidebar.warning('API key required to try this app.The API key is not stored in any form.')
+    st.sidebar.warning(
+        "API key required to try this app.The API key is not stored in any form."
+    )
     # st.stop()
 
-
 # Add a button to start a new chat
-st.sidebar.button("New Chat", on_click = new_chat, type='primary')
+st.sidebar.button("New Chat", on_click=new_chat, type="primary")
 
 # Get the user input
 user_input = get_text()
 
 # Generate the output using the ConversationChain object and the user input, and add the input/output to the session
 if user_input:
-    output = Conversation.run(input=user_input)  
-    st.session_state.past.append(user_input)  
-    st.session_state.generated.append(output)  
+    output = Conversation.run(input=user_input)
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(output)
 
 # Allow to download as well
 download_str = []
 # Display the conversation history using an expander, and allow the user to download it
 with st.expander("Conversation", expanded=True):
-    for i in range(len(st.session_state['generated'])-1, -1, -1):
-        st.info(st.session_state["past"][i],icon="🧐")
+    for i in range(len(st.session_state["generated"]) - 1, -1, -1):
+        st.info(st.session_state["past"][i], icon="🧐")
         st.success(st.session_state["generated"][i], icon="🤖")
         download_str.append(st.session_state["past"][i])
         download_str.append(st.session_state["generated"][i])
-    
+
     # Can throw error - requires fix
-    download_str = '\n'.join(download_str)
+    download_str = "\n".join(download_str)
     if download_str:
-        st.download_button('Download',download_str)
+        st.download_button("Download", download_str)
 
 # Display stored conversation sessions in the sidebar
 for i, sublist in enumerate(st.session_state.stored_session):
-        with st.sidebar.expander(label= f"Conversation-Session:{i}"):
-            st.write(sublist)
+    with st.sidebar.expander(label=f"Conversation-Session:{i}"):
+        st.write(sublist)
 
 # Allow the user to clear all stored conversation sessions
-if st.session_state.stored_session:   
+if st.session_state.stored_session:
     if st.sidebar.checkbox("Clear-all"):
         del st.session_state.stored_session
